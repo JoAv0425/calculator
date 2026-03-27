9c9060d310cfa6ef8be35d5253b7c6bf6e6627ed
JoAv0425 <praisecandomo@gmail.com>
2026-03-27 10:32:05 +0100

    Create variables and function for operation

--- calc.js
+++ calc.js
@@ -14,3 +14,19 @@ function divide(a,b) {
     return a / b;
 };
 
+
+let firstNum;
+let secondNum;
+let operator;
+
+function operate(operator,firstNum,secondNum) {
+    if (operator == "plus") {
+        add(firstNum,secondNum);
+    } else if (operator == "minus") {
+        subtract(firstNum,secondNum);
+    } else if (operator == "multiply") {
+        multiply(firstNum,secondNum);
+    } else if (operator == "divide") {
+        divide(firstNum,secondNum);
+    }
+};
