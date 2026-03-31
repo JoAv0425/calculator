c8fa7138a10bf752acf977142e8993d2c26f7d6a
JoAv0425 <praisecandomo@gmail.com>
2026-03-31 13:35:15 +0100

    Create first iteration for updating variables and display

--- calc.js
+++ calc.js
@@ -15,18 +15,47 @@ function divide(a,b) {
 };
 
 
-let firstNum;
-let secondNum;
-let operator;
+let firstNum = '';
+let secondNum = '';
+let operator = '';
 
 function operate(operator,firstNum,secondNum) {
-    if (operator == "plus") {
+    if (operator == "+") {
+        +firstNum;
+        +secondNum;
         add(firstNum,secondNum);
-    } else if (operator == "minus") {
+    } else if (operator == "-") {
+        +firstNum;
+        +secondNum;
         subtract(firstNum,secondNum);
-    } else if (operator == "multiply") {
+    } else if (operator == "×") {
+        +firstNum;
+        +secondNum;
         multiply(firstNum,secondNum);
-    } else if (operator == "divide") {
+    } else if (operator == "÷") {
+        +firstNum;
+        +secondNum;
         divide(firstNum,secondNum);
     }
 };
+
+const display = document.querySelector("div.display");
+const allBtns = document.querySelector(".buttons")
+function updateVariables(e) {
+    if (operator & firstNum & !isNaN(e.target.value)) {
+        secondNum += e.target.value;
+        display.textContent = `${firstNum} ${operator} ${secondNum}`;
+    } else if (!isNaN(e.target.value)) {
+        firstNum += e.target.value;
+        display.textContent = `${firstNum}`
+    } else if (e.target.value = '-'
+        || e.target.value == '+'
+        || e.target.value == "×"
+        || e.target.value == "÷"
+    ) {
+        operator = e.target.value;
+        display.textContent = `${firstNum} ${operator}`;
+    }
+
+}
+allBtns.addEventListener('click', updateVariables);
